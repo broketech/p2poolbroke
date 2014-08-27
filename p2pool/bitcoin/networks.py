22a63bb0dd6b33c20d5b99f62a8eacb21ab9d75e
broketech <broketech@nym.hush.com>
2014-08-27 15:59:51 -0700

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -448,6 +448,27 @@ parallelcoinsha=math.Object(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=1e8,
     ),
+    vikingcoin=math.Object(
+        P2P_PREFIX='55424b45'.decode('hex'), #messagestart
+        P2P_PORT=11066,
+        ADDRESS_VERSION=70, #pubkey_address
+        RPC_PORT=11067,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'Vikingaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 1*10000000 if height<1000 else 10*100000000 if height<2000 else 1*10000000 >> (height * 1)//100000,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=90, # s
+        SYMBOL='VIK',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'vikingcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/vikingcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vikingcoin'), 'vikingcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='',
+        ADDRESS_EXPLORER_URL_PREFIX='',
+        TX_EXPLORER_URL_PREFIX='',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=1e8,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
